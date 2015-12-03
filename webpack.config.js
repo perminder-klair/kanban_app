fc48e5ffdc892a4d7bc67e4a2aec4004088a21b1
parminder <perminder.klair@gmail.com>
2015-12-03 00:41:07 +0000

    deployment

--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,27 @@
 var path = require('path');
-var HtmlwebpackPlugin = require('html-webpack-plugin');
 var webpack = require('webpack');
+var HtmlwebpackPlugin = require('html-webpack-plugin');
 var merge = require('webpack-merge');
+var Clean = require('clean-webpack-plugin');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+var pkg = require('./package.json');
 
 const TARGET = process.env.npm_lifecycle_event;
-process.env.BABEL_ENV = TARGET;
-
 const PATHS = {
   app: path.join(__dirname, 'app'),
   build: path.join(__dirname, 'build')
 };
+
+process.env.BABEL_ENV = TARGET;
 
 var common = {
   entry: PATHS.app,
   resolve: {
     extensions: ['', '.js', '.jsx']
   },
-  output: {
-    path: PATHS.build,
-    filename: 'bundle.js'
-  },
   module: {
     loaders: [
-      {
-        test: /\.css$/,
-        loaders: ['style', 'css'],
-        include: PATHS.app
-      },
       {
         test: /\.jsx?$/,
         loaders: ['babel'],
@@ -36,26 +31,79 @@ var common = {
   },
   plugins: [
     new HtmlwebpackPlugin({
-      title: 'Karbon app'
+      title: 'Kanban app'
     })
   ]
 };
 
-if (TARGET === 'start' || !TARGET) {
+if(TARGET === 'start' || !TARGET) {
   module.exports = merge(common, {
     devtool: 'eval-source-map',
+    module: {
+      loaders: [
+        {
+          test: /\.css$/,
+          loaders: ['style', 'css'],
+          include: PATHS.app
+        }
+      ]
+    },
     devServer: {
       historyApiFallback: true,
       hot: true,
       inline: true,
       progress: true,
+
+      // display only errors to reduce the amount of output
       stats: 'errors-only',
-      quite: true,//disable console log messages
+
+      // parse host and port from env so this is easy
+      // to customize
       host: process.env.HOST,
       port: process.env.PORT
     },
     plugins: [
       new webpack.HotModuleReplacementPlugin()
+    ]
+  });
+}
+
+if(TARGET === 'build' || TARGET === 'stats' || TARGET === 'deploy') {
+  module.exports = merge(common, {
+    entry: {
+      app: PATHS.app,
+      vendor: Object.keys(pkg.dependencies)
+    },
+    output: {
+      path: PATHS.build,
+      filename: '[name].[chunkhash].js'
+    },
+    devtool: 'source-map',
+    module: {
+      loaders: [
+        {
+          test: /\.css$/,
+          loader: ExtractTextPlugin.extract('style', 'css'),
+          include: PATHS.app
+        }
+      ]
+    },
+    plugins: [
+      new Clean(['build']),
+      new ExtractTextPlugin('styles.[chunkhash].css'),
+      new webpack.optimize.CommonsChunkPlugin(
+        'vendor',
+        '[name].[chunkhash].js'
+      ),
+      new webpack.DefinePlugin({
+        // This affects react lib size
+        'process.env.NODE_ENV': JSON.stringify('production')
+      }),
+      new webpack.optimize.UglifyJsPlugin({
+        compress: {
+          warnings: false
+        }
+      })
     ]
   });
 }
